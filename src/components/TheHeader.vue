b27800aacf1018bfcaaf9caabd339b63cd2e18be
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2022-10-24 20:03:41 -0400

    🎨 IMPROVE: Add TheHeader

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="header">
+    test
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader',
+}
+</script>
+
+<style>
+.header {
+  background-color: #000;
+  color: #fff;
+}
+</style>
